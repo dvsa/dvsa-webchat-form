6e397734b7a12c0ff6113ea1e31d5b1cfacdb9eb
Shaun Hare <shaun.hare@dvsa.gov.uk>
2024-06-24 08:41:11 +0100

    updated input

--- javascripts/webchat-scripts.js
+++ javascripts/webchat-scripts.js
@@ -1,10 +1,10 @@
-let defaulturl = "https://www.timeforstorm.com/IM/endpoint/webchat2/5709/Webchat_test/0a1f18d5816353d746caf95e07366af4d496abff155f760cbb977210a3bc1d9d?";
-const defaultParams = "im_name=anon&im_email=anon%40dvsa.gov.uk&im_emailTemplate=3988&im_subject=DVSA";
+let defaulturl = "https://www.timeforstorm.com/IM/endpoint/webchat2/5709/DVSA%20Webchat/bd6de6a33eccfdccb3b722a450b5d735c1fd357ff323ff63c95b8f2a715dd27a?";
+const defaultParams = "im_name=anon&im_email=anon%40dvsa.gov.uk&im_emailTemplate=3988";
 export function init() {
     
     const url = new URL(window.location.href);
     const chatParams = url.searchParams;
-    const paramList = ['im_name', 'im_email', 'im_emailTemplate', 'im_subject'];
+    const paramList = ['im_name', 'im_email', 'im_emailTemplate'];
     if (chatParams) {
         url.searchParams.forEach((value,key) => {
             if (paramList.includes(key)) {
